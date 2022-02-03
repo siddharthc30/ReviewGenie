4b6ee3d1b2f7891e8c99a5ad663303f50c5f3439
Siddharth <siddharthcilamkoti@gmail.com>
2022-02-03 23:40:58 +0530

    Add test plot

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
-from flask import Flask, redirect, render_template,request
+from flask import Flask, redirect, render_template, request
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
 
 app = Flask(__name__)
 
+#home route
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-@app.route('/<asin>', methods = ["POST","GET"])
+#Dashboard of results route
+@app.route('/<asin>/dashboard', methods = ["POST","GET"])
 def dashboard(asin):
     asin = request.form.get("asin")
 
-    return render_template("test.html", asin = asin)
+    df = pd.DataFrame({
+      'Fruit': ['Apples', 'Oranges', 'Bananas', 'Apples', 'Oranges', 
+      'Bananas'],
+      'Amount': [4, 1, 2, 2, 4, 5],
+      'City': ['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal']
+    })
+    fig = px.bar(df, x='Fruit', y='Amount', color='City', barmode='group')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template("test.html", graphJSON = graphJSON)
+
+#Error page route
+@app.route('/error')
+def error():
+    return render_template('error.html')
 
 
+
+#strarting the app
 if __name__ == "__main__":
     app.run(debug = True)
