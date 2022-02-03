ba076424d61d60c4d5e2663b40903e09a78dc365
Siddharth <siddharthcilamkoti@gmail.com>
2022-02-03 20:13:44 +0530

    Fix form bug

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/<asin>', methods = ["POST","GET"])
+def dashboard(asin):
+    asin = request.form.get("asin")
+
+    return render_template("test.html", asin = asin)
+
 
 if __name__ == "__main__":
     app.run(debug = True)
