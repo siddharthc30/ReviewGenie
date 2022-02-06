6c4a0aa6a1d93657ef6a0fe56d8afbb5e3709689
Siddharth <siddharthcilamkoti@gmail.com>
2022-02-06 17:58:22 +0530

    Add graphs plot function

---
+++ plotGraphs.py
@@ -0,0 +1,52 @@
+import plotly.graph_objects as go
+
+# overall_sentiment = {
+#         'positive_score' : 0.7,
+#         'negative_score' : 0.3,
+#         'neutral_score' :  0
+#     }
+
+#funciton for plotting the pie chart of sentiment of whole product reviews
+def plot_pie(overall_sentiment):
+    labels = list(overall_sentiment.keys())
+    values = [overall_sentiment['positive_score'], overall_sentiment['negative_score'], overall_sentiment['neutral_score']]
+
+    fig = go.Figure(data =[go.Pie(labels = labels, values = values)])
+    fig.show()
+
+
+
+opinion = {
+    'hi' : [0.9, 0.1],
+    'hello' : [0.5, 0.5],
+    'this' : [0.2, 0.8]
+}
+
+#Function for plotting bar graph for the opinion and its trait
+def plot_bar(opinion):
+    words = list(opinion.keys())
+    positive = []
+    negative = []
+    for op in opinion.values():
+       positive.append(op[0])
+       negative.append(op[1])  
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Bar(
+        x = words,
+        y = positive,
+        name = 'Positive opinion'
+    ))
+
+    fig.add_trace(go.Bar(
+        x = words,
+        y = negative,
+        name = 'Negative opinion'
+    ))
+
+    fig.update_layout(barmode = 'group', bargap = 0.2, bargroupgap = 0.15)
+    fig.show()
+
+plot_bar(opinion)
+
